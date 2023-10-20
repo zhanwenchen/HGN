33df04e14d9eaa3d0fb12da42ad914d9f886c40f
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2023-10-20 18:09:46 -0400

    Use main method

--- scripts/3_prepare_para_sel.py
+++ scripts/3_prepare_para_sel.py
@@ -5,22 +5,23 @@ from pandas import DataFrame
 input_file = argv[1]
 output_file = argv[2]
 
-with open(input_file, 'r') as file_in:
-    input_data = json_load(file_in)
-labels, titles, contexts, questions = [], [], [], []
+if __name__ == '__main__':
+    with open(input_file, 'r') as file_in:
+        input_data = json_load(file_in)
+    labels, titles, contexts, questions = [], [], [], []
 
-for data in input_data:
-    gold_paras = [para for para , _  in data['supporting_facts']]
-    for entity, sentences in data['context']:
-        label = int(entity in gold_paras)
-        title = entity
-        context = " ".join(sentences)
+    for data in input_data:
+        gold_paras = [para for para , _  in data['supporting_facts']]
         question = data['question']
+        for entity, sentences in data['context']:
+            label = int(entity in gold_paras)
+            title = entity
+            context = " ".join(sentences)
 
-        labels.append(label)
-        titles.append(title)
-        contexts.append(context)
-        questions.append(question)
+            labels.append(label)
+            titles.append(title)
+            contexts.append(context)
+            questions.append(question)
 
-df = DataFrame({'title': titles, 'context': contexts, 'label': labels, 'question':questions})
-df.to_csv(output_file)
+    df = DataFrame({'title': titles, 'context': contexts, 'label': labels, 'question':questions})
+    df.to_csv(output_file)
